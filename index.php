b62fd2f2292853c067418ec40c704d503703f7e6
unknown <psiiriainen@HEL0PEKKAVANHA.emea.corp.jwt.com>
2015-11-05 09:59:11 +0200

    Nothing

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ use ThePaavero\CodeGen as CodeGen;
 require 'vendor/autoload.php';
 
 $config = [
-	'amount' => 350,
-	'codeLength' => 10,
-	'characters' => ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 1, 2, 3, 4, 5, 6, 7, 9],
-	'file' => 'codes.txt',
-	'append' => true
+    'amount' => 210,
+    'codeLength' => 8,
+    'characters' => ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 1, 2, 3, 4, 5, 6, 7, 9],
+    'file' => '210-codes-8-characters.txt',
+    'append' => true
 ];
 
 $codegen = new CodeGen\CodeGen($config);
